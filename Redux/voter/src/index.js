0bfc7831204228450a36eee5f76c04969b654d73
mpeng <mpeng@bridge2solutions.com>
2018-12-31 20:34:23 -0500

    Update

--- Redux/voter/src/index.js
+++ Redux/voter/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import configureStore from './store';
-
-import './index.css';
+import { createStore } from 'redux';
+import myApp from './reducers';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Results from './components/results';
 
-ReactDOM.render(
- <Provider store={configureStore()}>
-  <App />
- </Provider>,
- document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let store = createStore(myApp);
+function render() {
+  ReactDOM.render(
+    <div className="container">
+      <App store={store}/>
+      <hr />
+      <Results store={store}/>
+    </div>,
+    document.getElementById('root')
+  );
+}
+store.subscribe(render);
+render();
--- Redux/voter/src/store.js
+++ Redux/voter/src/store.js
@@ -8,3 +8,6 @@ export default function configureStore() {
    applyMiddleware(thunk)
  );
 }
+
+
+
